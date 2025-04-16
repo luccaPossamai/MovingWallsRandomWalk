12030708aa8285b28c5c989a9959a4f976ee4cc1
lpcamors <luccapmarcos@gmail.com>
2025-04-16 12:03:37 -0300

    updt 16.04.25

--- program.c
+++ program.c
@@ -7,11 +7,11 @@
 #define FORCE_SEED  0
 
 //  Temporal Evolution   #
-#define DETA        1e-4
+#define DETA        1e-2
 #define TMAX        1e7
 #define TMIN        1
 //  Non temporal Evolution
-#define TTICS       1
+#define TTICS       60
 #define LOOPS       1e5
 
 // For dEta Evolution
@@ -24,7 +24,7 @@
 #define COLLISION_RESET 0
 // Mode
 #define MODE        0 // 0 -> temporal
-                      // 1 -> correlation time
+                      // 1 -> w(deta)
 void simulate(void);
 void setup(void);
 void setupFile(char*, int);
@@ -32,11 +32,9 @@ void mergeTemporaryValues(void);
 int onTimeSimulate(float);
 void writeSpecifications(FILE*);
 void dEtaEvolution(void);
-void dEtaEvolutionDist(void);
 float timeForDEta(float, int);
 void takeMeasures(float);
 void onParticleUpdate(int);
-double wStatForDeta(float);
 
 
 
@@ -46,22 +44,20 @@ int xA, xB, xM, tempXA, tempXB, tempXM, W0 = 2;
 float dEta = DETA;
 double timeT;
 float etaA, etaB, lastMeasure = 0;
-int *distXm, *distXWalls, spectedWStat, boundDist;
 
 
 int main() {
     seed = setupRandom(FORCE_SEED ? 123456789 : 0);
 
     if(MODE == 0){
-        setupFile("data_rwmw_MEAN_w", 1);
-        dEtaEvolution();
-        fclose(fCompl);    
-    } else if(MODE == 2){
         setupFile("data_rwmw_W", 0);
         simulate();
         fclose(f);
-    } else if(MODE == 3){
-        dEtaEvolutionDist();
+
+    } else if(MODE == 1){
+        setupFile("data_rwmw_MEAN_w", 1);
+        dEtaEvolution();
+        fclose(fCompl);    
     }
     
     
@@ -124,46 +120,6 @@ void dEtaEvolution(void){
 }
 
 
-void dEtaEvolutionDist(void){
-    float *dEta_arr = smalloc(TTICS * sizeof(float));
-    if(SCALE){
-        geomProgression(dEta_arr, DETAMIN, DETAMAX, TTICS);
-    } else {
-        linearProgression(dEta_arr, DETAMIN, DETAMAX, TTICS);
-    }
-    for(int i = 0; i < TTICS; i++){
-        dEta = dEta_arr[i];
-        spectedWStat = wStatForDeta(dEta);
-        boundDist = spectedWStat;
-        distXm = smalloc((boundDist) * sizeof(int));
-        distXWalls = smalloc((boundDist) * sizeof(int));
-        for(int i = 0; i < boundDist ; i++){
-            distXm[i] = 0;
-            distXWalls[i] = 0;
-        }
-        char name[50] = "";
-        sprintf(name, "data_rwmw_dist_dEtae%.5f", dEta);
-        setupFile(name, 0);
-        setup();
-        float t_stat = timeForDEta(dEta, 1);
-        while(onTimeSimulate(t_stat)){}
-        
-        for(int i = 0; i < LOOPS; i++){
-            t_stat += timeForDEta(dEta, 0) + 1;
-            while(onTimeSimulate(t_stat)){}
-        }
-        for(int i = 0; i < boundDist; i++){
-            double pos = (i / (double) boundDist) * spectedWStat;
-            fprintf(f, "%.4f %.5f\n", pos, distXm[i] / ((float)(LOOPS)));
-        }
-        fclose(f);
-        
-        free(distXm); free(distXWalls);
-    }
-    free(dEta_arr);
-
-}
-
 void setup(){
     xA = (int)(-W0 / 2);
     xB = (int)( W0 / 2);
@@ -210,11 +166,8 @@ void writeSpecifications(FILE *file){
 	    case 0:
 	        fprintf(file, "# dEta: %.5f\n", dEta);
 	        fprintf(file, "# t, xA, xB, xM, W\n");
-	        break;
+	        break;  
 	    case 1:
-	        fprintf(file, "# t, W(0)W(t), W, W2\n");
-	        break;    
-	    case 2:
 	    
 	        fprintf(file, "# dEta: [%.5f, %.5f]\n", (float) DETAMIN, (float)DETAMAX);
 	        if(!isCompl){
@@ -222,13 +175,6 @@ void writeSpecifications(FILE *file){
 	        } else {
 	            fprintf(file, "# Measures: %d\n", (int) LOOPS);
 	            fprintf(file, "# dEta <W_stat>\n");
-	        }
-	        break;
-	    case 3:
-	        if(!isCompl){
-	            fprintf(file, "# dEta: %.5f\n", dEta);
-	            fprintf(file, "# Wstat: %d\n", spectedWStat);
-	            fprintf(file, "# x, p(xm = x), p(xW = x)\n");
 	        }
 	        break;
 	        
@@ -239,33 +185,13 @@ void writeSpecifications(FILE *file){
 float timeForDEta(float dEta, int forStat){
     return forStat ? 1.0e1 / (dEta * dEta) : 1.0e1 / dEta;
 }
-double wStatForDeta(float dEta){
-    return (2.0 / dEta) + 1;
-}
 
 void takeMeasures(float timeTarget){
     if(MODE == 0){
-        
         fprintf(f, "%.2f %d %d %d %d\n", timeTarget, xA, xB, xM, xB - xA);
     } else if(MODE == 1){
-        if(timeTarget == TMIN){
-            lastMeasure = xB - xA;
-        }
-
-        fprintf(f, "%.2f %d %d %d\n", timeTarget, (int)lastMeasure * (xA - xB), xB - xA, (xB - xA) * (xB - xA));
-        
-    } else if(MODE == 2){
         lastMeasure += xB - xA;
         fprintf(f, "%.2f %.5f %d\n", timeTarget, dEta, (xB - xA));
-    } else if(MODE == 3){
-        int w = xB - xA;
-        double pA = abs(xM - xA) / (double)w;
-        double pB = abs(xM - xB) / (double)w;
-        distXm[(int)(pA * boundDist)] += 1;
-        distXm[(int)(pB * boundDist)] += 1;
-        //double f = boundDist / (double)w;// pos / w = (0, 1/2)
-        //int index = (int)(2 * f * pos);
-        //distXm[index] += 1;
     }
     
 }
@@ -277,54 +203,32 @@ void onParticleUpdate(int particle){
             // decides on which direction the inner particle will walk
             p = (randomFloat() < .5) ? 1 : -1;
             tempXM += p;
-            // if it hits some of the outer particles these one gets pushed 
-            if(tempXM == tempXA) {
-                tempXA--;
-                etaA = 0;
-                if(COLLISION_RESET){
-                    tempXM -= p;
-                }
-            }
-            if(tempXM == tempXB) {
-                tempXB++;
-                etaB = 0;
-                if(COLLISION_RESET){
-                    tempXM -= p;
-                }
+            if(tempXM == tempXA || tempXM == tempXB){
+                tempXM -= p;
             }
             break;
         case 0:
-            etaA += dEta;
-            if(etaA >= 1.0){
+            if(tempXA + 1 == tempXM){
+                tempXA--;
                 etaA = 0;
-                // outer particles walks
-                tempXA++;
-                if(tempXA == tempXM){
-                    // if outer particles hits inner particle
-                    // it pushes the inner one
-                    tempXM++;
-                    if(tempXM == tempXB){
-                        // if the inner particle hits the other outer particle
-                        // return to the intial state
-                        tempXM--;
-                        tempXA--;
-                    }
+            } else {
+                etaA += dEta;
+                if(etaA >= 1.0){
+                    etaA = 0;
+                    tempXA++;    
                 }
             }
             break;
         case 2:
-            etaB += dEta;
-            if(etaB >= 1.0){
+            if(tempXB - 1 == tempXM){
+                tempXB++;
                 etaB = 0;
-                tempXB--;
-                
-                if(tempXB == tempXM){
-                    tempXM--;
-                    if(tempXM == tempXA) {
-                        tempXM++;
-                        tempXB++;
-                    }
-                }
+            } else {
+                etaB += dEta;
+                if(etaB >= 1.0){
+                    etaB = 0;
+                    tempXB--;
+                }    
             }
             break;
         
